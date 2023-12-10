4c204ed1047cea36b13e433b26aed04f8d08eaad
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2023-12-10 17:56:26 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <div id ="main-header">
+        <header>Logo</header>
+        <button>Cart</button>
+      </div>
+      
+      <main id="meals">
+        <ul className="meal-item">
+          <li>Food Items</li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
